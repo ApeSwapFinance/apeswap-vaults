17e215eed0ca8947ddd73ddb2498e4a5895e1677
Apeguru <ape.swap@hotmail.com>
2021-08-27 19:21:11 -0300

    refactor: New configs in migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,25 +6,38 @@ const StrategyMasterApeSingleFactory = artifacts.require("StrategyMasterApeSingl
 const StrategyKoalaFarmFactory = artifacts.require("StrategyKoalaFarmFactory");
 const { MasterChefVaults, MasterApeSingleVaults, MasterChefSingleVaults } = require('../configs/vaults');
 
-const adminAddress = "0x6c905b4108A87499CEd1E0498721F2B831c6Ab13";
-const routerAddress = "0xcf0febd3f17cef5b47b0cd257acf6025c5bff3b7";
+const config = {
+  bsc: {
+    adminAddress: "0x6c905b4108A87499CEd1E0498721F2B831c6Ab13",
+    routerAddress: "0xcf0febd3f17cef5b47b0cd257acf6025c5bff3b7",
+    vaultAddress: "0xd265954530a6942c80b22bab2a4c89c7335fcd6d",
+    usdAddress: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+    bananaAddress: "0x603c7f932ED1fc6575303D8Fb018fDCBb0f39a95",
+  }
+}
+
 let vaultAddress = "0xd265954530a6942c80b22bab2a4c89c7335fcd6d";
 const deployStrategies = true;
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network) {
   await deployer.deploy(VaultApe);
 
   const vaultApe = await VaultApe.deployed();
 
   vaultAddress = vaultApe.address;
 
-  await deployer.deploy(StrategyMasterChefSingleFactory, adminAddress, vaultAddress, routerAddress);
+  const adminAddress = config[network].adminAddress;
+  const routerAddress = config[network].routerAddress;
+  const usdAddress = config[network].usdAddress;
+  const bananaAddress = config[network].bananaAddress;
 
-  await deployer.deploy(StrategyMasterApeSingleFactory, adminAddress, vaultAddress, routerAddress);
+  await deployer.deploy(StrategyMasterChefSingleFactory, adminAddress, vaultAddress, routerAddress, usdAddress, bananaAddress);
 
-  await deployer.deploy(StrategyKoalaFarmFactory, adminAddress, vaultAddress, routerAddress);
+  await deployer.deploy(StrategyMasterApeSingleFactory, adminAddress, vaultAddress, routerAddress, usdAddress, bananaAddress);
 
-  await deployer.deploy(StrategyMasterChefFactory, adminAddress, vaultAddress, routerAddress);
+  await deployer.deploy(StrategyKoalaFarmFactory, adminAddress, vaultAddress, routerAddress, usdAddress, bananaAddress);
+
+  await deployer.deploy(StrategyMasterChefFactory, adminAddress, vaultAddress, routerAddress, usdAddress, bananaAddress);
 
   const strategyMasterChefSingleFactory = await StrategyMasterChefSingleFactory.deployed();
   const strategyMasterApeSingleFactory = await StrategyMasterApeSingleFactory.deployed();
