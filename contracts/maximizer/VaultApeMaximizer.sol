9d8463fa9691aa96086a2a9850c99512e8096bd5
Doublo54 <doublo54@gmail.com>
2022-01-30 13:57:57 +0100

    feat: Chainlink keepers compatible

--- contracts/maximizer/VaultApeMaximizer.sol
+++ contracts/maximizer/VaultApeMaximizer.sol
@@ -7,16 +7,26 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+
 import "../libs/IVaultApeMaximizer.sol";
 import "../libs/IStrategyMaximizer.sol";
 import "../libs/IBananaVault.sol";
 
-contract VaultApeMaximizer is IVaultApeMaximizer, ReentrancyGuard, Ownable {
+contract VaultApeMaximizer is
+    IVaultApeMaximizer,
+    ReentrancyGuard,
+    Ownable,
+    KeeperCompatibleInterface
+{
     using SafeERC20 for IERC20;
 
     address[] public vaults;
     mapping(address => bool) public strats;
     IBananaVault public BANANA_VAULT;
+
+    uint256 public immutable interval;
+    uint256 public lastTimeStamp;
 
     event AddPool(address indexed strat);
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
@@ -33,8 +43,10 @@ contract VaultApeMaximizer is IVaultApeMaximizer, ReentrancyGuard, Ownable {
         _;
     }
 
-    constructor(address _bananaVault) {
+    constructor(address _bananaVault, uint256 _updateInterval) {
         BANANA_VAULT = IBananaVault(_bananaVault);
+        interval = _updateInterval;
+        lastTimeStamp = block.timestamp;
     }
 
     function userInfo(uint256 _pid, address _user)
@@ -78,7 +90,7 @@ contract VaultApeMaximizer is IVaultApeMaximizer, ReentrancyGuard, Ownable {
         return stake;
     }
 
-    function earnAll() external override {
+    function earnAll() public override {
         for (uint256 i = 0; i < vaults.length; i++) {
             IStrategyMaximizer(vaults[i]).earn(0, 0, 0, 0);
         }
@@ -134,6 +146,28 @@ contract VaultApeMaximizer is IVaultApeMaximizer, ReentrancyGuard, Ownable {
     function harvestAll(uint256 _pid) external override nonReentrant {
         IStrategyMaximizer strat = IStrategyMaximizer(vaults[_pid]);
         strat.claimRewards(msg.sender, type(uint256).max);
+    }
+
+    // ===== Chainlink Keeper functions =====
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+    }
+
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        lastTimeStamp = block.timestamp;
+        earnAll();
     }
 
     // ===== OWNER functions =====
