689a5b8af192a44aeb1556cbbdd466b1e3bd2954
defifofum <defifofum0@gmail.com>
2022-02-08 14:29:10 -0500

    refactor: Add additional functions to IBananaVault

--- contracts/libs/IBananaVault.sol
+++ contracts/libs/IBananaVault.sol
@@ -20,5 +20,28 @@ interface IBananaVault is IAccessControlEnumerable {
 
     function deposit(uint256 _amount) external;
 
-    function withdraw(uint256 _amount) external;
+    function withdraw(uint256 _shares) external;
+
+    function lastHarvestedTime() external view returns (uint256);
+
+    function calculateTotalPendingBananaRewards()
+        external
+        view
+        returns (uint256);
+
+    function getPricePerFullShare() external view returns (uint256);
+
+    function available() external view returns (uint256);
+
+    function underlyingTokenBalance() external view returns (uint256);
+
+    function masterApe() external view returns (address);
+
+    function totalShares() external view returns (uint256);
+
+    function withdrawAll() external;
+
+    function treasury() external view returns (address);
+
+    function setTreasury(address _treasury) external;
 }
