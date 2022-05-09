95106a3aa087b0719015ca094cc51f4e9550f780
defifofum <defifofum0@gmail.com>
2022-05-09 10:34:45 -0400

    refactor: Update keeper migration

--- migrations/3_deploy_maximizer_vault.js
+++ migrations/3_deploy_maximizer_vault.js
@@ -1,95 +1,187 @@
 const BananaVault = artifacts.require("BananaVault");
 const KeeperMaximizerVaultApe = artifacts.require("KeeperMaximizerVaultApe");
 const StrategyMaximizerMasterApe = artifacts.require("StrategyMaximizerMasterApe");
+const RouterMock = artifacts.require("RouterMock");
+const BytesLib = artifacts.require("Bytes");
 const { getNetworkConfig } = require('../deploy-config')
+const { MAX_UINT256 } = require('@openzeppelin/test-helpers/src/constants');
+
+
+// NOTE: TESTNET MasterApe
+const strategyDeployments = [
+  {
+    farmPid: 7,
+    farmStakeTokenAddress: "0x30E74ceFD298990880758E20223f03129F52E699",  // HOR-NEY
+  },
+  {
+    farmPid: 8,
+    farmStakeTokenAddress: "0x4419D815c9c9329f9679782e76ec15bCe1B14a6D", // FOR-EVER
+  },
+]
+
+// NOTE MAINNET MasterApe
+// const strategyDeployments = [
+//   {
+//     farmPid: 1,
+//     farmStakeTokenAddress: "0xF65C1C0478eFDe3c19b49EcBE7ACc57BB6B1D713",  // BANANA-BNB
+//   },
+//   {
+//     farmPid: 2,
+//     farmStakeTokenAddress: "0x7Bd46f6Da97312AC2DBD1749f82E202764C0B914", // BANANA-BUSD
+//   },
+//   {
+//     farmPid: 45,
+//     farmStakeTokenAddress: "0x29A4A3D77c010CE100A45831BF7e798f0f0B325D", // MATIC-BNB
+//   },
+//   {
+//     farmPid: 49,
+//     farmStakeTokenAddress: "0x47A0B7bA18Bb80E4888ca2576c2d34BE290772a6", // FTM-BNB
+//   },
+//   {
+//     farmPid: 117,
+//     farmStakeTokenAddress: "0x119D6Ebe840966c9Cf4fF6603E76208d30BA2179", // CEEK-BNB
+//   },
+// ]
 
 module.exports = async function (deployer, network, accounts) {
-  const { adminAddress, masterApeAddress, bananaTokenAddress, apeFactory, apeRouter, wrappedNativeAddress } = getNetworkConfig(network, accounts);
-  const treasury = adminAddress;
+  let { adminAddress, masterApeAddress, bananaTokenAddress, treasuryAddress, apeRouterAddress, wrappedNativeAddress, libraries } = getNetworkConfig(network, accounts);
+  const tempAdmin = accounts[0];
 
+  if (tempAdmin == adminAddress) {
+    throw new Error(`tempAdmin (accounts[0]) ${tempAdmin} == adminAddress ${adminAddress}. This will cause role issues. Update deploy-config.js to new adminAddress.`)
+  }
+  /**
+   * Deploy mock Router (if needed)
+   */
+  if (network == 'development') {
+    console.log(`Deploying RouterMock for development`);
+    await deployer.deploy(RouterMock);
+    apeRouterAddress = (await RouterMock.deployed()).address;
+  }
+
+  /**
+   * Deploy BananaVault
+   */
   await deployer.deploy(BananaVault,
     bananaTokenAddress,
     masterApeAddress,
-    adminAddress
+    tempAdmin, // Using temp admin to manage roles during deployment
   );
   const bananaVault = await BananaVault.deployed();
 
+  /**
+   * Deploy / Link Libraries
+   */
+  let bytesLib;
+  if (libraries.bytes) {
+    console.log(`libraries.bytes is set. Using ${libraries.bytes} for Bytes library.`)
+    bytesLib = await BytesLib.at(libraries.bytes);
+  } else {
+    console.log('libraries.bytes not set in deploy-config. Deploying new Bytes library.')
+    await deployer.deploy(BytesLib);
+    bytesLib = await BytesLib.deployed();
+  }
+  await deployer.link(bytesLib, KeeperMaximizerVaultApe);
+
+  /**
+   * Deploy KeeperMaximizerVaultApe
+   */
+  const MaximizerSettings = {
+    treasury: treasuryAddress,
+    keeperFee: 50, // .5%
+    platform: treasuryAddress,
+    platformFee: 0,
+    buyBackRate: 0,
+    withdrawFee: 10, // .1%
+    withdrawFeePeriod: MAX_UINT256,
+    withdrawRewardsFee: 0,
+  }
   await deployer.deploy(KeeperMaximizerVaultApe,
-    adminAddress,
-    adminAddress,
+    adminAddress, // Keeper
+    tempAdmin, // Owner (Using temp admin to add strategies during deployment)
     bananaVault.address,
-    86400,
-    [treasury, 50, treasury, 0, 0, 25, 259200, 100]);
+    86400, // Compound max delay (1day)
+    [
+      MaximizerSettings.treasury,
+      MaximizerSettings.keeperFee,
+      MaximizerSettings.platform,
+      MaximizerSettings.platformFee,
+      MaximizerSettings.buyBackRate,
+      MaximizerSettings.withdrawFee,
+      MaximizerSettings.withdrawFeePeriod,
+      MaximizerSettings.withdrawRewardsFee,
+    ]
+  );
   const keeperMaximizerVaultApe = await KeeperMaximizerVaultApe.deployed();
 
-  await deployer.deploy(StrategyMaximizerMasterApe,
-    masterApeAddress,
-    0,
-    true,
-    bananaTokenAddress,
-    bananaTokenAddress,
-    bananaVault.address,
-    apeRouter,
-    [bananaTokenAddress],
-    [bananaTokenAddress, wrappedNativeAddress],
-    [
-      adminAddress,
-      keeperMaximizerVaultApe.address
-    ]
-  );
-  const strategyMaximizerMasterApe = await StrategyMaximizerMasterApe.deployed();
-
-  //HOR-NEY
-  await deployer.deploy(StrategyMaximizerMasterApe,
-    masterApeAddress,
-    7,
-    false,
-    "0x30E74ceFD298990880758E20223f03129F52E699",
-    bananaTokenAddress,
-    bananaVault.address,
-    apeRouter,
-    [bananaTokenAddress],
-    [bananaTokenAddress, wrappedNativeAddress],
-    [
-      adminAddress,
-      keeperMaximizerVaultApe.address
-    ]
-  );
-  const strategyMaximizerMasterApe1 = await StrategyMaximizerMasterApe.deployed();
-
-  //FOR-EVER
-  await deployer.deploy(StrategyMaximizerMasterApe,
-    masterApeAddress,
-    8,
-    false,
-    "0x4419D815c9c9329f9679782e76ec15bCe1B14a6D",
-    bananaTokenAddress,
-    bananaVault.address,
-    apeRouter,
-    [bananaTokenAddress],
-    [bananaTokenAddress, wrappedNativeAddress],
-    [
-      adminAddress,
-      keeperMaximizerVaultApe.address
-    ]
-  );
-  const strategyMaximizerMasterApe2 = await StrategyMaximizerMasterApe.deployed();
-
-
-  // Define roles and grant roles
+  /**
+   * Define roles and grant roles
+   */
+  this.DEFAULT_ADMIN_ROLE = await bananaVault.DEFAULT_ADMIN_ROLE();
   this.MANAGER_ROLE = await bananaVault.MANAGER_ROLE();
-  await bananaVault.grantRole(this.MANAGER_ROLE, keeperMaximizerVaultApe.address, { from: adminAddress });
+  await bananaVault.grantRole(this.MANAGER_ROLE, keeperMaximizerVaultApe.address, { from: tempAdmin });
+  await bananaVault.grantRole(this.DEFAULT_ADMIN_ROLE, adminAddress, { from: tempAdmin });
+  await bananaVault.grantRole(this.MANAGER_ROLE, adminAddress, { from: tempAdmin });
+  await bananaVault.renounceRole(this.DEFAULT_ADMIN_ROLE, tempAdmin, { from: tempAdmin });
+  await bananaVault.renounceRole(this.MANAGER_ROLE, tempAdmin, { from: tempAdmin });
+  const bananaVaultOutput = {
+    bananaVaultAddress: bananaVault.address,
+    MaximizerHasManagerRole: await bananaVault.hasRole(this.MANAGER_ROLE, keeperMaximizerVaultApe.address),
+    adminHasAdminRole: await bananaVault.hasRole(this.DEFAULT_ADMIN_ROLE, adminAddress),
+    adminHasManagerRole: await bananaVault.hasRole(this.MANAGER_ROLE, adminAddress),
+    tempAdminHasAdminRole: await bananaVault.hasRole(this.DEFAULT_ADMIN_ROLE, tempAdmin),
+    tempAdminHasManagerRole: await bananaVault.hasRole(this.MANAGER_ROLE, tempAdmin),
+  }
 
-  //Add vault
-  await keeperMaximizerVaultApe.addVault(strategyMaximizerMasterApe.address, { from: adminAddress });
-  await keeperMaximizerVaultApe.addVault(strategyMaximizerMasterApe1.address, { from: adminAddress });
-  await keeperMaximizerVaultApe.addVault(strategyMaximizerMasterApe2.address, { from: adminAddress });
+  /**
+   * Deploy strategies
+   */
+  let strategyOutput = [];
+  for (const strategyDeployment of strategyDeployments) {
+    const { farmPid, farmStakeTokenAddress } = strategyDeployment;
+    const isBananaStaking = farmStakeTokenAddress == bananaTokenAddress;
+    await deployer.deploy(StrategyMaximizerMasterApe,
+      masterApeAddress,
+      farmPid,
+      isBananaStaking,
+      farmStakeTokenAddress,
+      bananaTokenAddress,
+      bananaVault.address,
+      apeRouterAddress,
+      [bananaTokenAddress],
+      [bananaTokenAddress, wrappedNativeAddress],
+      [
+        adminAddress,  // owner
+        keeperMaximizerVaultApe.address // vaultApe address
+      ]
+    );
+    const strategyMaximizerMasterApe = await StrategyMaximizerMasterApe.deployed();
+    await keeperMaximizerVaultApe.addVault(strategyMaximizerMasterApe.address, { from: tempAdmin });
+    strategyOutput.push({
+      strategyMaximizerMasterApe: strategyMaximizerMasterApe.address,
+      farmPid,
+      farmStakeTokenAddress,
+      isBananaStaking,
+    })
+  }
 
+  await keeperMaximizerVaultApe.transferOwnership(adminAddress, { from: tempAdmin });
+  const keeperOutput = {
+    keeperMaximizerVaultApeAddress: keeperMaximizerVaultApe.address,
+    keeperOwner: await keeperMaximizerVaultApe.owner(),
+    bytesLib: BytesLib.address,
+  }
+
+  /**
+   * Log output
+   */
   console.dir({
-    bananaVault: bananaVault.address,
-    keeperMaximizerVaultApe: keeperMaximizerVaultApe.address,
-    masterApeAddress,
     adminAddress,
-    treasury,
+    masterApeAddress,
+    keeperOutput,
+    bananaVaultOutput,
+    MaximizerSettings,
+    withdrawFeePeriod: MaximizerSettings.withdrawFeePeriod.toString(),
+    strategyOutput,
   })
 };
